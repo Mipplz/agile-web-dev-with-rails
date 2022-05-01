b8ab157cb846eca83b2417ac7e3d8fb911921232
Mipplz <evilv99@gmail.com>
2022-05-01 23:03:04 +0200

    Task:F partial

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,8 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
   def index
     @products = Product.order :title
-
-    if session[:counter].nil?
-      session[:counter] = 0
-    end
-    session[:counter] += 1
-    @counter = session[:counter]
   end
 end
